34bf6958592c3ea4369a8d46b99b95ea419647c9
Christopher Scrantom <christopher.scrantom@gmail.com>
2021-06-14 19:43:41 -0500

    WIP

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -6,27 +6,89 @@ import Show from './../Show';
 
 const testShow = {
     //add in approprate test data structure here.
+	name: 'Castlevania',
+	summary: 'Neat netflix original that brings the classic games to an animated narrative',
+	seasons: [
+		{
+			id: 1,
+			name: 'Season 1',
+			episodes: [1,2,3]
+		},
+		{
+			id: 2,
+			name: 'Season 2',
+			episodes: []
+		},
+		{
+			id: 3,
+			name: 'Season 3',
+			episodes: []
+		}
+	]
 }
 
 test('renders testShow and no selected Season without errors', ()=>{
+	render(<Show show={testShow} selectedSeason={"none"}/>)
 });
 
 test('renders Loading component when prop show is null', () => {
+	render(<Show show={null}/>)
+
+	const loading = screen.queryByTestId(/loading-container/i)
+
+	expect(loading).toBeInTheDocument();
+	expect(loading).toBeTruthy();
+	expect(loading).toBeVisible();
 });
 
 test('renders same number of options seasons are passed in', ()=>{
+	render(<Show show={testShow} selectedSeason={"none"}/>)
+
+	const options = screen.getAllByTestId('season-option')
+
+	expect(options).toHaveLength(3);
 });
 
 test('handleSelect is called when an season is selected', () => {
+	const mockHandleSelect = jest.fn();
+
+	render(<Show show={testShow} selectedSeason={"none"} handleSelect={mockHandleSelect}/>)
+
+	const dropDown = screen.getByLabelText(/select a season/i)
+	userEvent.selectOptions(dropDown, [screen.getByText('Season 1')]);
+
+	expect(mockHandleSelect).toHaveBeenCalledTimes(1);
 });
 
 test('component renders when no seasons are selected and when rerenders with a season passed in', () => {
+	const { rerender } = render(<Show show={testShow} selectedSeason={"none"}/>)
+
+	render(<Show show={testShow} selectedSeason={"none"}/>)
+
+	const episodes = screen.queryByTestId('episodes-container')
+	// ?????? are we supposed to modify episode to see if the episode we query for exists? I could only see a testId working here. 
+
+	expect(episodes).toBeFalsy();
+
+	
+	// console.log(testShow.seasons[0])
+	rerender(<Show show={testShow} selectedSeason={1}/>)
+	
+	// ????? breaks here
+
+	// const dropDown = screen.getByLabelText(/select a season/i)
+	// userEvent.selectOptions(dropDown, [(screen.getAllByTestId('season-option'))[0]]);
+	// console.log(episode)
+	// expect(episodes).toBeInTheDocument();
+
 });
 
 //Tasks:
 //1. Build an example data structure that contains the show data in the correct format. A show should contain a name, a summary and an array of seasons, each with a id, name and (empty) list of episodes within them. Use console.logs within the client code if you need to to verify the structure of show data.
 //2. Test that the Show component renders when your test data is passed in through show and "none" is passed in through selectedSeason.
-//3. Test that the Loading component displays when null is passed into the show prop (look at the Loading component to see how to test for it's existance)
+//3. Test that the Loading component displays when null is passed into the show prop (look at the Loading component to see how to test for it's existence)
 //4. Test that when your test data is passed through the show prop, the same number of season select options appears as there are seasons in your test data.
+
 //5. Test that when an item is selected, the handleSelect function is called. Look at your code to see how to get access to the select Dom element and userEvent reference materials to see how to trigger a selection.
+
 //6. Test that the episode component DOES NOT render when the selectedSeason props is "none" and DOES render the episode component when the selectedSeason prop has a valid season index.
